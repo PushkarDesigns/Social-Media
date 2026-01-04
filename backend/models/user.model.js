6251f59cbd7fc90de5b939ecce2e31ae94560351
Pushkar <rawatprince815@gmail.com>
2026-01-04 16:28:54 +0530

    making user model...

---
+++ backend/models/user.model.js
@@ -0,0 +1,59 @@
+import mongoose, { mongo } from "mongoose";
+
+// user model 
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      require: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      require: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      require: true,
+    },
+    uploadImage: {
+      type: String,
+      default: "",
+    },
+    about: {
+      type: String,
+      default: "",
+    },
+    gender: {
+      type: String,
+      enum: ["male", "female", "Rather not say"],
+    },
+    followers: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    following: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    posts: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Post",
+      },
+    ],
+    bookmarks: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Post",
+      },
+    ],
+  },{ timestamps: true }
+);
+
+export const User = mongoose.model("User", userSchema);
