9eab9ba39d29be2d0879828009d39be790bfb967
Pushkar <rawatprince815@gmail.com>
2026-01-04 18:28:36 +0530

    making register the user login and sign up user and creating the token to store the password in cookies ...

---
+++ backend/controllers/user.controller.js
@@ -0,0 +1,71 @@
+import { User } from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+// user register / singhup
+export const register = async (req, res) => {
+  try {
+    const { username, email, password } = req.body; // object destruction
+    if (!username || !email || !password) {
+      return res.status(401).json({
+        message: "Something is missing, Please check!",
+        success: false,
+      });
+    }
+    const user = await User.findOne({ email });
+    if (user) {
+      return res.status(401).json({
+        message: "Try different email",
+        success: false,
+      });
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+    await User.create({
+      username,
+      email,
+      password: hashedPassword,
+    });
+    return res.status(201).json({
+      message: "Account Created Successfully.",
+      success: true,
+    });
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+// user login / sign in
+
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res.status(401).json({
+        message: "Something is missing, please check!",
+        success: false,
+      });
+    }
+    let user = await user.findOne({email});
+    if (!user) {
+      return res.status(401).json({
+        message: "Incorrect email or password",
+        success: false,
+      });
+    }
+    const isPasswordMatch = await bcrypt.compare(password, user.password);
+    if (!isPasswordMatch) {
+      return res.status(401).json({
+        message: "Incorrect email or password",
+        success: false,
+      });
+    }
+
+    const token = await jwt.sign({userId:user._id},process.env.SECRET_KEY,{expriesIn:'10d'});
+    return res.cookie('token', token, {httpOnly:true, samesite:'strict', maxAge: 10*24*60*60*1000}).json({
+        message: `Welcome back ${user.username}`,
+        success:true,
+    })
+  } catch (error) {
+    console.log(error);
+  }
+};
