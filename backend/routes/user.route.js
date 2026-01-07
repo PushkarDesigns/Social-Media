717733fc1edf2db35ce80062018f0fd52e97a3f3
Pushkar <rawatprince815@gmail.com>
2026-01-07 13:28:31 +0530

    Add user authentication and profile management API routes...

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -3,7 +3,10 @@ import express from "express";
 
 // Import controller functions for login, logout, and register from a specific file path.
 import {
+  editProfile,
+  followOrUnfollow,
   getProfile,
+  getSuggestedUsers,
   login,
   logout,
   register
@@ -11,6 +14,7 @@ import {
 
 // Import a middleware function used for authenticating requests.
 import isAuthenticated from "../middlewares/isAuthenticated.js";
+import upload from "../middlewares/multer.js";
 
 // Create a new router instance from express.
 const router = express.Router();
@@ -28,4 +32,11 @@ router.route("/logout").get(logout);
 // This route is protected by the 'isAuthenticated' middleware, meaning a valid token/session
 // is required before the subsequent (unseen) profile controller logic runs.
 router.route("/:id/profile").get(isAuthenticated,getProfile /* , profileControllerFunction */ );
-router.route("/profile/edit").get(isAuthenticated, /* , profileControllerFunction */ );
+router.route("/profile/edit").post(isAuthenticated, upload.single('uploadImage'), editProfile /* , profileControllerFunction */ );
+router.route("/suggested").get(isAuthenticated, getSuggestedUsers); // isAuthenticated: This function is likely used for authentication, verifying that the user making the request is logged in before allowing them access to the route.
+// getSuggestedUsers: This is the route handler that runs if the user is authenticated, fetching and returning a list of suggested users.
+router.route('/followorunfollow/:id').post(isAuthenticated, followOrUnfollow);  // This sets up a POST endpoint at the path /followorunfollow/, which also expects a dynamic id parameter in the URL (e.g., /followorunfollow/123).
+// It also uses the isAuthenticated middleware for authentication.
+// followOrUnfollow: This is the route handler that executes if the user is authenticated. It presumably uses the provided id to either follow or unfollow the specified user
+
+export default router
