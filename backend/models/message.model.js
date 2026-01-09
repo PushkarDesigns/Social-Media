4ba284dc44b328ec17fea3de228eb934f50b9461
Pushkar <rawatprince815@gmail.com>
2026-01-09 20:44:29 +0530

    making message model...

--- backend/models/message.model.js
+++ backend/models/message.model.js
@@ -18,4 +18,5 @@ const messageSchema = new mongoose.Schema({
     },
 });
 
-export default Message = mongoose.model("Message", messageSchema);
+const Message = mongoose.model("Message", messageSchema);
+export default Message;
