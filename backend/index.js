336f60f05a0bd5967cb1c1d2fdcdc636eeddb02e
Pushkar <rawatprince815@gmail.com>
2026-01-09 21:25:54 +0530

    mongodb connected and Setup base application routing for posts and messages...

--- backend/index.js
+++ backend/index.js
@@ -1,20 +1,22 @@
-import express, { urlencoded } from 'express';
-import cors from 'cors';
-import cookieParser from 'cookie-parser';
-import connectDB from './utils/db.js';
-import userRoute from './routes/user.route.js';
-import dotenv from 'dotenv';
+import express, { urlencoded } from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import connectDB from "./utils/db.js";
+import userRoute from "./routes/user.route.js";
+import postRoute from "./routes/post.route.js";
+import messageRoute from "./routes/message.route.js";
+import dotenv from "dotenv";
 dotenv.config({});
 
-const PORT = process.env.PORT || 8000
+const PORT = process.env.PORT || 8000;
 
 const app = express();
 
 app.get("/", (req, res) => {
-    return res.status(200).json({
-        message: "I'm coming from backend",
-        success: true
-    });
+  return res.status(200).json({
+    message: "I'm coming from backend",
+    success: true,
+  });
 });
 
 //middlewares
@@ -22,8 +24,8 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(urlencoded({ extended: true }));
 const corsOptions = {
-    origin: 'http://localhost:5173',
-    credentials: true
+  origin: "http://localhost:5173",
+  credentials: true,
 };
 
 app.use(cors(corsOptions));
@@ -31,9 +33,10 @@ app.use(cors(corsOptions));
 // api
 app.use("/api/v1/user", userRoute);
 // "http://localhost:3000/api/v1/user/register"
+app.use("/api/v1/post", postRoute);
+app.use("/api/v1/message", messageRoute);
 
-
-app.listen(PORT,() => {
-    console.log(`Server listen at port ${PORT}`)
-    connectDB();
-})
+app.listen(PORT, () => {
+  console.log(`Server listen at port ${PORT}`);
+  connectDB();
+});
