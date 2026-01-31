7bc2c08b60f247139cb8ffe93da3beb2b50b80ae
Pushkar <rawatprince815@gmail.com>
2026-01-31 17:21:18 +0530

    user personal page build up...

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -2,16 +2,25 @@ import { Avatar, AvatarFallback, AvatarImage } from '@/ui/avatar'
 import useGetUserProfile from '@/hooks/useGetUserProfile';
 import { useParams } from 'react-router-dom';
 import { useSelector } from 'react-redux';
+import { Badge } from './ui/badge';
+import { AtSign } from 'lucide-react';
+import { useState } from 'react';
 
 const Profile = () => {
   const params = useParams();
   const userId = params.id;
   useGetUserProfile(userId);
+  const [ activeTab, setActiveTab ] = useState('posts');
 
   const userProfile = useSelector(store => store.auth);
   // console.log(userProfile);
   const isLoggedInUserProfile = false;
   const isFollowing = false;
+
+  const handlerTabChange = (tab) => {
+    setActiveTab(tab);
+  }
+  const displayedPost = activeTab === 'posts' ? userProfile?.posts : userProfile?.bookmarks;
   return (
     <div className='flex max-w-5xl justify-center mx-auto pl-10'>
       <div className='flex flex-col gap-20 p-8'>
@@ -27,17 +36,51 @@ const Profile = () => {
               <div className='flex items-center gap-2'>
                 <span>{userProfile?.username}</span>
                 {
-                  isLoggedInUserProfile ? (<>
-                  <Button variant='secondary' className='hover:bg-gray-200 h-8'>Edit profile</Button>
-                  <Button variant='secondary' className='hover:bg-gray-200 h-8'>View archive</Button>
-                  <Button variant='secondary' className='hover:bg-gray-200 h-8'>Ad tools</Button>
-                </>) : (
-                  <Button className='bg-[#0095f6] hover:bg-[#3192d2] h-8'>Follow</Button>
-                )
+                  isLoggedInUserProfile ? (
+                    <>
+                      <Button variant='secondary' className='hover:bg-gray-200 h-8'>Edit profile</Button>
+                      <Button variant='secondary' className='hover:bg-gray-200 h-8'>View archive</Button>
+                      <Button variant='secondary' className='hover:bg-gray-200 h-8'>Ad tools</Button>
+                    </>
+                  ) : (
+                    isFollowing ? (
+                      <>
+                        <Button variant='secondary' className='h-8'>Unfollow</Button>
+                        <Button variant='secondary' className='h-8'>Message</Button>
+                      </>
+                    ) : (
+                      <Button className='bg-[#0695F6] hover:bg-[#3192d2] h-8'>Follow</Button>
+                    )
+                  )
                 }
+              </div>
+              <div className='flex items-center gap-4'>
+                <p><span className='font-semibold'> {userProfile?.posts.length} </span>posts</p>
+                <p><span className="font-semibold"> {userProfile?.followers.length} </span>followers</p>
+                <p><span className='font-semibold'> {userProfile?.following.length} </span>following</p>
+              </div>
+              <div className='flex flex-col gap-1'>
+                <span className='font-semibold'> {userProfile?.bio || 'bio here...'}</span>
+                <Badge className="w-fit" variant='secondary'><AtSign /> <span className='pl-1'> {userProfile?.username}</span></Badge>
+                <span>& Learn code with patel mernstack style</span>
+                <span>& Turing code into fun</span>
+                <span>DM for collaboration</span>
               </div>
             </div>
           </section>
+        </div>
+        <div className='border-t border-t-gray-200'>
+          <div className='flex items-center justify-center gap-10 text-sm'>
+            <span className={`py-3 cursor-pointer ${activeTab === 'posts' ? 'font-bold' : ''}`} onClick={()=>handlerTabChange('posts')}>
+              POSTS
+            </span>
+            <span className={`py-3 cursor-pointer ${activeTab === 'saved' ? 'font-bold' : ''}`} onClick={()=>handlerTabChange('saved')}>
+              SAVED
+            </span>
+            <span className='py-3 cursor-pointer'>REELS</span>
+            <span className='py-3 cursor-pointer'>TAGS</span>
+          </div>
+          
         </div>
       </div>
     </div>
